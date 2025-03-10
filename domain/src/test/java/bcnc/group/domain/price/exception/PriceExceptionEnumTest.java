3e058ce2385dad14c72c38b9c1cf5579a3dcb8dc
anzu <alexarasanz@gmail.com>
2025-03-10 19:51:39 +0100

    move product price exception into model package

--- domain/src/test/java/bcnc/group/domain/price/exception/PriceExceptionEnumTest.java
+++ domain/src/test/java/bcnc/group/domain/price/exception/PriceExceptionEnumTest.java
@@ -4,27 +4,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import bcnc.group.domain.price.model.exception.ProductPriceExceptionEnum;
+
 class PriceExceptionEnumTest {
 
   @Test
   void testPriceExceptionEnum() {
-    assertEquals("Price must be a positive number", PriceExceptionEnum.PRICE_MUST_BE_A_POSITIVE_NUMBER
+    assertEquals("Price must be a positive number", ProductPriceExceptionEnum.PRICE_MUST_BE_A_POSITIVE_NUMBER
         .getMessage());
-    assertEquals("PriceList must be a positive number", PriceExceptionEnum.PRICE_LIST_MUST_BE_A_POSITIVE_NUMBER
+    assertEquals("PriceList must be a positive number", ProductPriceExceptionEnum.PRICE_LIST_MUST_BE_A_POSITIVE_NUMBER
         .getMessage());
-    assertEquals("Currency must be a valid currency", PriceExceptionEnum.CURRENCY_MUST_BE_A_VALID_CURRENCY
+    assertEquals("Currency must be a valid currency", ProductPriceExceptionEnum.CURRENCY_MUST_BE_A_VALID_CURRENCY
         .getMessage());
-    assertEquals("StartDate must be a valid date", PriceExceptionEnum.START_DATE_MUST_BE_A_VALID_DATE
+    assertEquals("StartDate must be a valid date", ProductPriceExceptionEnum.START_DATE_MUST_BE_A_VALID_DATE
         .getMessage());
-    assertEquals("EndDate must be a valid date", PriceExceptionEnum.END_DATE_MUST_BE_A_VALID_DATE
+    assertEquals("EndDate must be a valid date", ProductPriceExceptionEnum.END_DATE_MUST_BE_A_VALID_DATE
         .getMessage());
-    assertEquals("PriceId must be a positive number", PriceExceptionEnum.PRICE_ID_MUST_BE_A_POSITIVE_NUMBER
+    assertEquals("PriceId must be a positive number", ProductPriceExceptionEnum.PRICE_ID_MUST_BE_A_POSITIVE_NUMBER
         .getMessage());
-    assertEquals("ProductId must be a positive number", PriceExceptionEnum.PRODUCT_ID_MUST_BE_A_POSITIVE_NUMBER
+    assertEquals("ProductId must be a positive number", ProductPriceExceptionEnum.PRODUCT_ID_MUST_BE_A_POSITIVE_NUMBER
         .getMessage());
-    assertEquals("BrandId must be a positive number", PriceExceptionEnum.BRAND_ID_MUST_BE_A_POSITIVE_NUMBER
+    assertEquals("BrandId must be a positive number", ProductPriceExceptionEnum.BRAND_ID_MUST_BE_A_POSITIVE_NUMBER
         .getMessage());
-    assertEquals("Priority must be a positive number", PriceExceptionEnum.PRIORITY_MUST_BE_A_POSITIVE_NUMBER
+    assertEquals("Priority must be a positive number", ProductPriceExceptionEnum.PRIORITY_MUST_BE_A_POSITIVE_NUMBER
         .getMessage());
   }
 }
