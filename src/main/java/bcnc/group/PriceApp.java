480e8f9721b27fc7cb6c4ca003852b8aa8eee639
anzu <alexarasanz@gmail.com>
2025-03-11 22:53:12 +0100

    add spring boot application entry point

---
+++ src/main/java/bcnc/group/PriceApp.java
@@ -0,0 +1,13 @@
+package bcnc.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceApp.class, args);
+    }
+
+}
