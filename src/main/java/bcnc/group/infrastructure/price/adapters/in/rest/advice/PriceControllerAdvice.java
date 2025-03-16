3d722ec99e19ddf291dd0f6af47bdaf061abd2f5
anzu <alexarasanz@gmail.com>
2025-03-16 19:04:12 +0100

    return custom error response in controller advice

--- src/main/java/bcnc/group/infrastructure/price/adapters/in/rest/advice/PriceControllerAdvice.java
+++ src/main/java/bcnc/group/infrastructure/price/adapters/in/rest/advice/PriceControllerAdvice.java
@@ -1,5 +1,7 @@
 package bcnc.group.infrastructure.price.adapters.in.rest.advice;
 
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,18 +9,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import bcnc.group.domain.price.model.exception.ProductPriceException;
 import bcnc.group.domain.price.model.exception.ProductPriceNotFoundException;
+import bcnc.group.openapi.model.CommonError;
 
 @ControllerAdvice
 public class PriceControllerAdvice {
 
     @ExceptionHandler(ProductPriceException.class)
-    public ResponseEntity<String> handleEmptyInput(ProductPriceException exception) {
-        return new ResponseEntity<>(exception.getErrorMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<CommonError> handleEmptyInput(ProductPriceException exception) {
+        return new ResponseEntity<>(new CommonError().code(HttpStatus.BAD_REQUEST.value())
+                .message(exception.getErrorMessage()).timestamp(LocalDateTime.now()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ProductPriceNotFoundException.class)
-    public ResponseEntity<String> handleProductPriceNotFoundException(ProductPriceNotFoundException exception) {
-        return new ResponseEntity<>(exception.getErrorMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<CommonError> handleProductPriceNotFoundException(ProductPriceNotFoundException exception) {
+        return new ResponseEntity<>(new CommonError().code(HttpStatus.NOT_FOUND.value())
+                .message(exception.getErrorMessage()).timestamp(LocalDateTime.now()), HttpStatus.NOT_FOUND);
     }
 
 }
