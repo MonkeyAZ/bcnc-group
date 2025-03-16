df7bef39b62abb5e0245e8783be74bd4f1eb1428
anzu <alexarasanz@gmail.com>
2025-03-16 19:03:53 +0100

    improve performance in query

--- src/main/java/bcnc/group/infrastructure/price/adapters/out/jpa/repository/JpaProductPriceRepository.java
+++ src/main/java/bcnc/group/infrastructure/price/adapters/out/jpa/repository/JpaProductPriceRepository.java
@@ -1,7 +1,7 @@
 package bcnc.group.infrastructure.price.adapters.out.jpa.repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,10 +10,17 @@ import bcnc.group.infrastructure.price.adapters.out.jpa.entity.ProductPriceEntit
 
 public interface JpaProductPriceRepository extends JpaRepository<ProductPriceEntity, Long> {
 
-    @Query("SELECT pp FROM ProductPriceEntity pp " +
-            "WHERE pp.productId = :productId " +
-            "AND pp.brandId = :brandId " +
-            "AND (pp.startDate <= :date AND pp.endDate >= :date)")
-    List<ProductPriceEntity> getPriceByProductBrandAndDate(Long productId, Long brandId, LocalDateTime date);
+    @Query(value = "SELECT p1.* FROM PRICES as p1 " +
+            "JOIN" +
+            "(SELECT BRAND_ID, PRODUCT_ID, MAX(PRIORITY) PRIORITY " +
+            "FROM PRICES " +
+            "WHERE BRAND_ID = :brandId AND PRODUCT_ID = :productId AND START_DATE <= :date AND END_DATE >= :date " +
+            "GROUP BY BRAND_ID, PRODUCT_ID) as p2 " +
+            "ON  " +
+            "p1.BRAND_ID = p2.BRAND_ID AND " +
+            "p1.PRODUCT_ID = p2.PRODUCT_ID AND " +
+            "p1.PRIORITY = p2.PRIORITY " +
+            "WHERE p1.START_DATE <= :date AND p1.END_DATE >= :date", nativeQuery = true)
+    Optional<ProductPriceEntity> getPriceByProductBrandAndDate(Long productId, Long brandId, LocalDateTime date);
 
 }
